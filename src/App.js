20984f797b09f3986b638f5526f85c6fb23deb86
Nini Antadze <nini.antadze@gmail.com>
2024-01-24 16:44:42 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer>
+        This Websited Is Coded By{" "}
+        <a href="https://www.linkedin.com/in/nino-antadze/" target="_blank">
+          Ninia
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/niniantadze/react-dictionary-by-ninia"
+          target="_blank"
+        >
+          Github!
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
