aee434889ad98787fff267f6cd16a5eb64b3f8ab
Nini Antadze <nini.antadze@gmail.com>
2024-01-24 18:55:12 +0100

    integrated API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
-
+import axios from "axios";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}  `);
+
+    let apiKey = "fcec8ad3dft937afe4da000c469b9obf";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
