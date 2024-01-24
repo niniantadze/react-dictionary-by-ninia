aefd2241773e6ed8a1b8e284c2a710b1fd506ae2
Nini Antadze <nini.antadze@gmail.com>
2024-01-24 17:53:45 +0100

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,7 +6,7 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for  ${keyword}  `);
+    alert(`Searching for ${keyword}  `);
   }
 
   function handleKeywordChange(event) {
